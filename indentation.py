5477058c6c254b0b8eb4b0aa4950393a4f8838e2
rami20-meet <rami20@meet.mit.edu>
2018-08-01 05:27:36 -0400

    Stopping here

---
+++ indentation.py
@@ -0,0 +1,8 @@
+new_fruit = input('What fruit am I sorting?')
+
+if _____________:
+	print('Bin 1')
+elif ___________:
+	print('Bin 2')
+else:
+	print('Error! I do not recognise this fruit!')
---
+++ turtle_party.py
@@ -0,0 +1,14 @@
+strawberries = 30
+is_weekend = True
+
+if is_weekend:
+    if strawberries < 39:
+        print('not fun!')
+    else:
+        print('fun!')
+
+else:
+    if strawberries > 61 or strawberries < 39:
+        print('Not Fun!')
+    else:
+        print('Fun!')
